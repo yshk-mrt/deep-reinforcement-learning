32b420f12d504354fff8f2e435871794c2bf0301
yshk-mrt <argentine_backbreker67@msn.com>
2019-10-28 21:58:17 +0100

    Day9: Mini project Taxi-v2, Score 9.33

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -12,6 +12,29 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.epsilon = 0.005
+        self.gamma = 0.99
+        self.alpha = 0.1
+        
+        # Result
+        # Episode 20000/20000 || Best average reward 9.333
+        # self.epsilon = 0.005
+        # self.gamma = 0.99
+        # self.alpha = 0.1
+        # Expected-SARSA
+        
+
+    def select_epsilon_greedy_action(self, state):
+        action = np.random.choice(np.arange(self.nA), p=self.get_probs(self.Q[state], self.epsilon, self.nA)) \
+                      if state in self.Q else np.random.choice(np.arange(self.nA))
+        return action
+
+    def get_probs(self, Q_s, epsilon, nA):
+        """ obtains the action probabilities corresponding to epsilon-greedy policy """
+        policy_s = np.ones(nA) * epsilon / nA
+        best_a = np.argmax(Q_s)
+        policy_s[best_a] = 1 - epsilon + (epsilon / nA)
+        return policy_s
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,7 +47,7 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        return self.select_epsilon_greedy_action(state)
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -37,4 +60,10 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        # [Q-Learning]
+        # next_action = np.argmax(self.Q[next_state])
+        # self.Q[state][action] += self.alpha*(reward + self.gamma*(0 if done else self.Q[next_state][next_action]) - self.Q[state][action])
+        
+        # [Expected-SARSA]
+        best_action = np.argmax(self.Q[next_state])
+        self.Q[state][action] += self.alpha*(reward + self.gamma*(0 if done else (self.Q[next_state][best_action]*(1.0 - self.epsilon) + self.epsilon/self.nA*np.sum(self.Q[next_state]))) - self.Q[state][action])
